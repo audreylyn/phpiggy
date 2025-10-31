17739cb5d9b7780a865019495e1d515cfe0aeeb3
audreylyn <audreylynvillanueva1504@gmail.com>
2025-10-31 16:22:18 +0800

    Initial commit

---
+++ src/App/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__ . "/../../vendor/autoload.php";
+
+use Framework\App;
+
+$app = new App();
+
+$app->get('/');
+$app->get('about/team');
+$app->get('/about/team');
+$app->get('/about/team/');
+
+dd($app);
+
+return $app;
